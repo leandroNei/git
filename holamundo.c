0d548e5ba00878a15de4c85d686448385051d8b5
leandronei <leandroignacio.neilaf@alumnos.ulagos.cl>
2020-09-10 16:33:54 -0300

    Cambio commit rapido

--- holamundo.c
+++ holamundo.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
 
-    printf(" ¡¡¡Hola Mundo, Bienvenido a un nuevo semestre papa, vas a ser el mejor ing. civil en informatica!!! ");
-
+    printf("  Hola Mundo, Bienvenido a un nuevo semestre papa, vas a ser el mejor ing. civil en informatica!!! ");
+    return 0;
 }
